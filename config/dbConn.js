0d3f87037f18c9b302cdbf06b74dedeca3fcf6bd
bariss48 <berkk.baris48@gmail.com>
2023-01-09 11:27:38 +0300

    db url change

--- config/dbConn.js
+++ config/dbConn.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     const handleDbString = process.env.DATABASE_URI;
-    await mongoose.connect(handleDbString); //
+    await mongoose.connect(`${handleDbString}`);
   } catch (err) {
     console.log(err);
   }
